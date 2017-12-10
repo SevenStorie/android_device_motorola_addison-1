bc3948074af1478e88529cbec983008fb4827fb6
DK Kira <dinix.hot@gmail.com>
2017-12-10 00:28:23 -0200

    Update validus.mk

--- validus.mk
+++ validus.mk
@@ -1,3 +1,4 @@
+
 #
 # Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2017 The LineageOS Project
@@ -32,3 +33,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=motorola/addison/addison:7.1.1/NPNS26.118-22-1/1:user/release-keys \
     PRIVATE_BUILD_DESC="addison-user 7.1.1 NPNS26.118-22-1 1 release-keys" \
     PRODUCT_NAME="Moto Z Play"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	DEVICE_MAINTAINERS="Kagami"
